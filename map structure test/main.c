3c3e297fb15fcf6064a3f7edb7b459b17b362582
HANSEOL <nothinonthere@icoud.com>
2023-08-07 15:45:29 +0900

    Initial Commit

---
+++ map structure test/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  map structure test
+//
+//  Created by 한설 on 2023/08/07.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
